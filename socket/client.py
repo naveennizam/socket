cf1d4c9768d94b252729de4110f2113d3bafcd4e
Naveen Nizam <naveennizam01@gmail.com>
2022-10-05 20:27:57 -0700

    ok

--- socket/client.py
+++ socket/client.py
@@ -1,29 +1,41 @@
+
 import socket
-import sys
-import os
-import subprocess as sb
+from colorama import Fore  , Style
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()  # '192.168.3.105'
 port = 5050
-
 s.connect((host,port))
 
-while True:
-    
-    data = s.recv(4096)
-    print(data.decode(('utf-8')))
-    
-    
-    # if data [ : 2].decode("utf-8") == 'cd':
-    #     os.chdir(data[3:].decode("utf-8"))
 
-    # if len(data) > 0:
-    #     cmd = sb.Popen(data[:].decode(("utf-8")),shell=True,stdout=sb.PIPE,stdin=sb.PIPE,stderr=sb.PIPE)
-    #     output_bytes = cmd.stdout.read()+cmd.stderr.read()
-    #     output_str = str(output_bytes,'utf-8')
-    #     currentWD = os.getcwd() + '> '
-    #     s.send(str.encode(output_str + currentWD))
 
-    #     print(output_str)
-       # s.close()
+data = s.recv(1024)
+we=(data.decode(('utf-8')))
+
+res = we.strip('][,').split(',')
+print("The number of packets recieved is: {} \n"  .format(str(len(res))))
+
+([list((i,res[i])) for i in range(len(res))])# to make list with index & value
+for i in range(len(res)):
+    print(res[i])
+
+pre = res[5] 
+k = 5
+res[k] = -1
+
+for j in range(len(res)):
+             
+    print("Received frame is: " , res[j])
+    
+
+for j in range(len(res)):               
+
+    if (res[j] == -1): 
+        print("Request to retransmit packet no " , (j+1) , " again!!")
+        print()    
+        print(Fore.YELLOW,"Received frame is: " , pre)
+        print(Style.RESET_ALL)
+       
+print("quit, System close!")
+
+
--- socket/server.py
+++ socket/server.py
@@ -1,47 +1,46 @@
+
 ##############################
 ##### PYTHON NETWORKING ######
 ##############################
 
 import socket
-import sys 
+import sys
 
 def create_socket():
     try:
-        
+        global a
+        a = [30, 40, 50, 60, 70, 80, 90, 100, 110]
         global host
         global port 
         global s
         host = ""
         port = 5050 # not use it
-        s = socket.socket()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as msg:
         print("socket error" + str(msg))
 #########################################
 # binding the socket with listening and connection
+'''
+is it enough to connect computer with socket? NO
+Why do need bindto a host & port with socket?
+it might be able to open line of communication with socket,
+but it's really need to know about info about computer or device
+it is supposed to communicating with
 
-# is it enough to connect computer with socket? NO
-# Why do need bindto a host & port with socket?
-# i might be able to open line of communication with socket,
-# but it's really need to know about info about computer or device
-# it is supposed to communicating with
-
-
+'''
 def bind_socket():
-    
-        global a
+    try:
         global host
         global port 
         global s
         
-        print("Binding the port   " + str(port))
-      
-
+        print("Binding the port   " , (port))
         s.bind((host,port))
-        s.listen()
+        s.listen(5)
 
-    # except socket.error as msg:
-    #     print("socket binding error"+ str(msg)+"\n"+"retrying...")
-    #     bind_socket()
+    except socket.error as msg:
+        print("socket binding error"+ str(msg)+"\n"+"retrying...")
+        bind_socket()
 
 ################################
 
@@ -50,24 +49,30 @@ def bind_socket():
 def socket_accept():
     conn, addr = s.accept() #(object of connection,list Ip to a port)
     print("connectin has been established  "+ " | IP | "+ addr[0]+" | port | "+str(addr[1]))
-    send_command(conn) # here conn for conversation
-    #conn.close()
+    send_command(conn) 
+
 #############################################
 
 # Send command to client/victim or friend
 
 def send_command(conn):
     while True:
-        cmd = input()
-        if cmd == 'quit':
-            conn.close()
-            s.close()
-            sys.exit() # to close command prompt
-        if len(str.encode(cmd)) >0:
+        print("The number of packets sent is: {} \n" .format(str(len(a))))
+        
+        y = []
+        for i in range(len(a)):
+          
+            print(a[i])
+            y+=[a[i]]
+            z = (str(y))
             
-            conn.send(str.encode(cmd))
-            client_response = str(conn.recv(1024),"utf-8")
-            print(client_response,end="")
+            z=(z.encode())
+        conn.send(bytearray(z))
+
+        conn.close()
+        sys.exit()
+
+    
 ###############################################
 
 if __name__ == '__main__' :
